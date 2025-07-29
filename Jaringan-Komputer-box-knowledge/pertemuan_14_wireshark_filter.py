4fb57216de197bb4076f70577bfb9b6d1ce62e49
Hafizh Hilman Asyhari <81144031+hafizhhasyhari@users.noreply.github.com>
2025-07-29 23:56:51 +0700

    Create pertemuan_14_wireshark_filter_Simulation-teks.py

---
+++ Jaringan-Komputer-box-knowledge/pertemuan_14_wireshark_filter.py
@@ -0,0 +1,4 @@
+# pertemuan_14_wireshark_filter.py
+print("Contoh filter Wireshark:")
+print("tcp.port == 80 → untuk melihat trafik HTTP")
+print("ip.addr == 192.168.1.1 → untuk alamat IP tertentu")
