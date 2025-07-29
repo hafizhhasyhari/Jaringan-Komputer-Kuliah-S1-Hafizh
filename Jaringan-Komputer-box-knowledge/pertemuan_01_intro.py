7fe750da3ab8eeabdd98c9f39ce58b3dbceec01a
Hafizh Hilman Asyhari <81144031+hafizhhasyhari@users.noreply.github.com>
2025-07-29 23:50:55 +0700

    Create pertemuan_01_intro.py

---
+++ Jaringan-Komputer-box-knowledge/pertemuan_01_intro.py
@@ -0,0 +1,4 @@
+# pertemuan_01_intro.py
+print("Pengenalan Jaringan Komputer")
+print("Jaringan komputer adalah kumpulan komputer yang saling terhubung.")
+print("Contoh sederhana adalah koneksi LAN di lab komputer.")
