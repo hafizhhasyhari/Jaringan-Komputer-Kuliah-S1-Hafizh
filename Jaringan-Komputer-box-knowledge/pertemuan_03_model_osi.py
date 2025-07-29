887690a097e11b168c571c336680cf40602d83b6
Hafizh Hilman Asyhari <81144031+hafizhhasyhari@users.noreply.github.com>
2025-07-29 23:51:29 +0700

    Create pertemuan_03_model_osi.py

---
+++ Jaringan-Komputer-box-knowledge/pertemuan_03_model_osi.py
@@ -0,0 +1,14 @@
+# pertemuan_03_model_osi.py
+lapisan_osi = [
+    "1. Physical",
+    "2. Data Link",
+    "3. Network",
+    "4. Transport",
+    "5. Session",
+    "6. Presentation",
+    "7. Application"
+]
+
+print("Model OSI terdiri dari 7 lapisan:")
+for layer in lapisan_osi:
+    print(layer)
